ba84d28f7adcd817fee5a441388037ef9fabebbe
Erick Galvez <erickgalvez54@yahoo.com>
2021-12-22 18:51:20 -0500

    improve icon hover functionality

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // Erick Galvez main page actions / fucntions
 // 12 - 16 - 2021
 
-// get icon and add hover event to change to what will be 'darkmode'
+// get icon in order to change class
 let icon = document.querySelector('#colorIcon i');
-//icon.className = 'fas fa-moon';
 
+// when cursor enters icon field change icon
 icon.addEventListener("mouseenter", function (event) {
     event.target.className = 'far fa-moon fa-2x';
+}, false);
 
-    setTimeout(function () {
-        event.target.className = 'fas fa-moon fa-2x';
-    }, 800);
+// when cursor leaves icon field change icon back to original state
+icon.addEventListener("mouseleave", function (event) {
+    event.target.className = 'fas fa-moon fa-2x';
 }, false);
