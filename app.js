d6582a2ed0b21f4522bc3ba880a8f858c9676fc5
Erick Galvez <erickgalvez54@yahoo.com>
2021-12-22 18:03:42 -0500

    Change text font, test display properties and add hover effect to icon

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // Erick Galvez main page actions / fucntions
 // 12 - 16 - 2021
+
+// get icon and add hover event to change to what will be 'darkmode'
+let icon = document.querySelector('#colorIcon i');
+//icon.className = 'fas fa-moon';
+
+icon.addEventListener("mouseenter", function (event) {
+    event.target.className = 'far fa-moon fa-2x';
+
+    setTimeout(function () {
+        event.target.className = 'fas fa-moon fa-2x';
+    }, 800);
+}, false);
